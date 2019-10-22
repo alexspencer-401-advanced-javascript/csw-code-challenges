1fc8e793f654f044d638753bdb959971bcf3fd74
Alex Spencer <alexander.j.spencer@gmail.com>
2019-10-22 08:03:00 -0700

    tests all passing

--- test/dateAdder.test.js
+++ test/dateAdder.test.js
@@ -15,11 +15,41 @@ describe('Date add testing', () => {
     const minutes = dateAdder(date, diffMinutes);
     expect(minutes.toString()).toEqual('Sun Jan 20 2019 10:43:30 GMT-0800 (Pacific Standard Time)');
   });
+  
   it('can successfully add hours', () => {
     const date = new Date(2018, 12, 20, 10, 33, 30, 0);
     const diffHours = '10h';
     const hours = dateAdder(date, diffHours);
-    expect(hours.toString()).toEqual('Sun Jan 20 2019 20:33:30 GMT-0800 (Pacific Standard Time)')
+    expect(hours.toString()).toEqual('Sun Jan 20 2019 20:33:30 GMT-0800 (Pacific Standard Time)');
+  });
+
+  it('can successfully add days', () => {
+    const date = new Date(2018, 12, 20, 10, 33, 30, 0);
+    const diffHours = '10d';
+    const hours = dateAdder(date, diffHours);
+    expect(hours.toString()).toEqual('Wed Jan 30 2019 10:33:30 GMT-0800 (Pacific Standard Time)');
+  });
+
+  it('can successfully add weeks', () => {
+    const date = new Date(2018, 12, 20, 10, 33, 30, 0);
+    const diffHours = '1w';
+    const hours = dateAdder(date, diffHours);
+    expect(hours.toString()).toEqual('Sun Jan 27 2019 10:33:30 GMT-0800 (Pacific Standard Time)');
+  });
+
+  it('can successfully add months', () => {
+    const date = new Date(2018, 12, 20, 10, 33, 30, 0);
+    const diffHours = '1M';
+    const hours = dateAdder(date, diffHours);
+    expect(hours.toString()).toEqual('Wed Feb 20 2019 10:33:30 GMT-0800 (Pacific Standard Time)');
+  });
+
+  it('can successfully add years', () => {
+    const date = new Date(2018, 12, 20, 10, 33, 30, 0);
+    console.log(date);
+    const diffHours = '1y';
+    const hours = dateAdder(date, diffHours);
+    expect(hours.toString()).toEqual('Mon Jan 20 2020 10:33:30 GMT-0800 (Pacific Standard Time)');
   });
 
 });
