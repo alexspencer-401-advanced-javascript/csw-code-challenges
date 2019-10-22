5af7cc1d6f405b0a8e47ee13afba5e7a08826763
Alex Spencer <alexander.j.spencer@gmail.com>
2019-10-22 15:52:10 -0700

    cleaning code

--- revenue-calc/revenueCalc.js
+++ revenue-calc/revenueCalc.js
@@ -1,13 +1,10 @@
 function dailyRev(transactions) {
-
   let obj = {};
   
   for(let i = 0; i < transactions.length; i++) {
     const date = new Date(transactions[i].timestamp);
-    
     const formattedDate = date.toUTCString().slice(date.length - 16, 16);
     const newDate = new Date(formattedDate).toDateString();
-
     if(!obj[newDate]) {
       obj[newDate] = transactions[i].price;
     } else obj[newDate] += transactions[i].price;
